278652259802fcefce265aaf5363f32eddb4b17c
Abraham Guo <aguo@precisionplanting.com>
2025-03-06 08:17:59 -0600

    build

--- dist/index.cjs
+++ dist/index.cjs
@@ -611,7 +611,7 @@ var init_public = __esm({
 // src/main/version.evaluate.cjs
 var require_version_evaluate = __commonJS({
   "src/main/version.evaluate.cjs"(exports2, module2) {
-    module2.exports = "3.6.0-0d12cf259";
+    module2.exports = "3.6.0-68a540cf9";
   }
 });
 
