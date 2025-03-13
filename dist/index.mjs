bfeb1bf927d7b97e9806aed877cc5d8d896820ea
Abraham Guo <aguo@precisionplanting.com>
2025-03-13 07:22:27 -0500

    npm up

--- dist/index.mjs
+++ dist/index.mjs
@@ -21447,7 +21447,7 @@ var object_omit_default = omit;
 import * as doc from "./doc.mjs";
 
 // src/main/version.evaluate.cjs
-var version_evaluate_default = "3.6.0-73e3e6151";
+var version_evaluate_default = "3.6.0-f90033421";
 
 // src/utils/public.js
 var public_exports = {};
