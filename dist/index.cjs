6e08309b1863702181cc968285d9c397f266cd6b
Abraham Guo <aguo@precisionplanting.com>
2025-04-07 23:14:19 -0500

    npm up

--- dist/index.cjs
+++ dist/index.cjs
@@ -611,7 +611,7 @@ var init_public = __esm({
 // src/main/version.evaluate.cjs
 var require_version_evaluate = __commonJS({
   "src/main/version.evaluate.cjs"(exports2, module2) {
-    module2.exports = "3.6.0-6eea3c831";
+    module2.exports = "3.6.0-8ac8c45f9";
   }
 });
 
--- dist/index.mjs
+++ dist/index.mjs
@@ -21484,7 +21484,7 @@ var object_omit_default = omit;
 import * as doc from "./doc.mjs";
 
 // src/main/version.evaluate.cjs
-var version_evaluate_default = "3.6.0-6eea3c831";
+var version_evaluate_default = "3.6.0-8ac8c45f9";
 
 // src/utils/public.js
 var public_exports = {};
