6aeb3e6fab060bac57e2bafc83aafa50e553652c
Abraham Guo <aguo@precisionplanting.com>
2025-04-07 23:15:00 -0500

    build

--- dist/index.mjs
+++ dist/index.mjs
@@ -21484,7 +21484,7 @@ var object_omit_default = omit;
 import * as doc from "./doc.mjs";
 
 // src/main/version.evaluate.cjs
-var version_evaluate_default = "3.6.0-8ac8c45f9";
+var version_evaluate_default = "3.6.0-5a17bc3f8";
 
 // src/utils/public.js
 var public_exports = {};
