778a4b4264225e18cfaa80950d9090fbb22c6869
Abraham Guo <aguo@precisionplanting.com>
2025-04-28 15:04:15 -0500

    build

--- dist/prettier/index.mjs
+++ dist/prettier/index.mjs
@@ -21122,7 +21122,7 @@ var object_omit_default = omit;
 import * as doc from "./doc.mjs";
 
 // src/main/version.evaluate.js
-var version_evaluate_default = "3.6.0-f963755ee";
+var version_evaluate_default = "3.6.0-bece6caa9";
 
 // src/utils/public.js
 var public_exports = {};
